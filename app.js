c7e1591ba9abe9ad3ee69eedc8c710b1d358c69b
LokendraBhat <lokendrabhat.2022@gmail.com>
2024-02-23 14:40:21 +0545

    node app

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+require ('dotenv').config();
+// or
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8001;
+
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello nodejs\n');
+});
+
+// /name route
+var USER = process.env.USER;
+app.get('/name', (req, res) => {
+  const name = req.query.name || 'World';
+  res.send(`Hello ${USER} \n`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
